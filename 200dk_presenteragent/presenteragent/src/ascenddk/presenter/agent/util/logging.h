27ef43521c194b18948283ce35a3a56a45df12ac
HuaweiAscend <ascend@huawei.com>
2020-07-13 19:00:23 +0800

    update 200dk_presenteragent/presenteragent/src/ascenddk/presenter/agent/util/logging.h.

--- 200dk_presenteragent/presenteragent/src/ascenddk/presenter/agent/util/logging.h
+++ 200dk_presenteragent/presenteragent/src/ascenddk/presenter/agent/util/logging.h
@@ -34,24 +34,20 @@
 #ifndef ASCENDDK_PRESENTER_AGENT_UTIL_LOGGING_H_
 #define ASCENDDK_PRESENTER_AGENT_UTIL_LOGGING_H_
 
-#include "acl/acl_base.h"
+
 #include "cerrno"
 
 
 // debug level logging
-#define AGENT_LOG_DEBUG(fmt, ...) \
-  aclAppLog(ACL_DEBUG, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define AGENT_LOG_DEBUG  printf 
 
 // info level logging
-#define AGENT_LOG_INFO(fmt, ...) \
-  aclAppLog(ACL_INFO, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define AGENT_LOG_INFO  printf
 
 // warn level logging
-#define AGENT_LOG_WARN(fmt, ...) \
-  aclAppLog(ACL_WARNING, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define AGENT_LOG_WARN  printf
 
 // error level logging
-#define AGENT_LOG_ERROR(fmt, ...) \
-  aclAppLog(ACL_ERROR, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define AGENT_LOG_ERROR  printf   
 
 #endif /* ASCENDDK_PRESENTER_AGENT_UTIL_LOGGING_H_ */
